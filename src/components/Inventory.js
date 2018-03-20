27e9358f845d27acf6b40e23742a2fa552ca928c
Shun Miyaki <shunmiyaki@hotmail.com>
2018-03-20 14:38:28 +0900

    14 and 15 complete

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,6 +7,7 @@ class Inventory extends React.Component {
       <div className="inventory">
         <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button> {/* loadSample made in app.js since it's where the fish state is. */}
       </div>
     )
   }
