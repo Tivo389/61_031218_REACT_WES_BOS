1b19051e98efdd2e8f16ed48c00aca6819f5880a
Shun Miyaki <shunmiyaki@hotmail.com>
2018-05-27 22:16:34 +0900

    config Added

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  apiKey: "AIzaSyDrdYRG7g3nvP8evnBl9CHIs5N1pT0PLk8",
+  authDomain: "catch-of-the-day-tivo389.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-tivo389.firebaseio.com"
+};
+
+export default config;
